2ab73365cc0a01f2e9a5f47263d2b59a61716db7
Liwen Chen <clw.air@gmail.com>
2015-08-19 13:09:37 +1000

    s

---
+++ numericset.js
@@ -0,0 +1,41 @@
+/*
+given several single figures
+find out expressions like abc * ed so that numbers in:
+abc
+ed
+abc * e
+abc * d
+abc * ed
+are all the comosition of given figures
+*/
+
+var n = Number(process.argv[2]);
+var count = 0;
+
+for (var abc = 111; abc <= 999; abc ++)
+{
+	for (var de = 11; de <= 99; de ++)
+	{
+		var x = abc * (de % 10);
+		var y = abc * Math.floor(de / 10);
+		var z = abc * de;
+		var s = '' + abc + de + x + y + z;
+		var yes = true;
+		var aim = n.toString();
+		for (var i = 0; i < s.length; i ++)
+		{
+			if (aim.indexOf(s.charAt(i)) === -1)
+			{
+				yes = false;
+				break;
+			}
+		}
+		if (yes)
+		{
+			console.log('<' + (++count) + '>');
+			console.log('  ' + abc + '\n' + 'X  ' + de + '\n-----\n ' + x + '\n' + y + '\n-----\n' + z + '\n\n');
+		}
+	}
+}
+
+console.log('The number of solutions = ' + count);
---
+++ snakelike.js
@@ -0,0 +1,55 @@
+/*
+given n, output a serpentine matrix
+e.g. n = 4:
+10 11 12 1
+9  16 13 2
+8  15 14 3
+7  6  5  4
+*/
+
+var n = Number(process.argv[2]);
+var a = [];
+
+for(var i = 0; i < n; i ++)
+{
+	a[i] = [];
+	for (var j = 0; j < n; j ++)
+	{
+		a[i][j] = 0;
+	}
+}
+
+var total = 1;
+var x = 0;
+var y = n - 1;
+a[0][n - 1] = 1;
+
+while (total < Math.pow(n, 2))
+{
+	while ((x + 1 < n) && (a[x + 1][y] === 0))
+	{
+		a[++x][y] = ++ total;
+	}
+	while ((y - 1 >= 0) && (a[x][y - 1] === 0))
+	{
+		a[x][--y] = ++ total;
+	}
+	while ((x - 1 >= 0) && (a[x - 1][y] === 0))
+	{
+		a[--x][y] = ++ total;
+	}
+	while ((y + 1 < n) && (a[x][y + 1] === 0))
+	{
+		a[x][++y] = ++ total;
+	}
+}
+
+for (var i = 0; i < n; i ++)
+{
+	var s = '';
+	for (var j = 0; j < n; j ++)
+	{
+		s += (a[i][j] + "  ").slice(0, 3);
+	}
+	console.log(s);
+}
