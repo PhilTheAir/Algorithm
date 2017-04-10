70ffe111d4cafbf3de93baa9b296c57425ab3fc6
Phil Chen <clw.air@gmail.com>
2017-04-11 09:16:35 +1000

    Update numericset.js

--- numericset.js
+++ numericset.js
@@ -14,7 +14,7 @@ var count = 0;
 
 for (var abc = 111; abc <= 999; abc ++) {
 	for (var de = 11; de <= 99; de ++) {
-		var x = abc * (de % 10); 
+		var x = abc * (de % 10);  
 		var y = abc * Math.floor(de / 10); 
 		var z = abc * de;
 		var s = '' + abc + de + x + y + z; 
