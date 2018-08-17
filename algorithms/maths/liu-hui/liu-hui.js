ade0728bda72fa7254986f55094bb4e1c369a6ad
Phil Chen <clw.air@gmail.com>
2018-08-17 14:12:19 +1000

    s

---
+++ algorithms/maths/liu-hui/liu-hui.js
@@ -0,0 +1,27 @@
+const circleRadius = 1;
+
+const getNGonSideLength = (sideLength, splitCounter) => {
+  if (splitCounter <= 0) {
+    return sideLength;
+  }
+  const halfSide = sideLength / 2;
+  const perpendicular = Math.sqrt((circleRadius ** 2) - (halfSide ** 2));
+  const excessRadius = circleRadius - perpendicular;
+  const splitSideLength = Math.sqrt((excessRadius ** 2) + (halfSide ** 2));
+  return getNGonSideLength(splitSideLength, splitCounter - 1);
+}
+
+const getNGonSideCount = splitCount => {
+  const hexagonSidesCount = 6;
+  const nGonSideCount = hexagonSidesCount * (splitCount ? 2 ** splitCount : 1);
+  return nGonSideCount;
+}
+
+export const liuHui = (splitCount = 1) => {
+  const nGonSideLength = getNGonSideLength(circleRadius, splitCount - 1);
+  const nGonSideCount = getNGonSideCount(splitCount - 1);
+  const nGonPerimeter = nGonSideLength * nGonSideCount;
+  const approximateCircleArea = (nGonPerimeter / 2) * circleRadius;
+  const pie = approximateCircleArea / (circleRadius ** 2);
+  return pie;
+}
